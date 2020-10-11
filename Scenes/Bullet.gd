4235128a2d1637b44a6f5d110b35e2e6bb07c213
davhallac1 <davidehallac@gmail.com>
2020-10-11 10:04:09 -0400

    Added Projectile Shooting

---
+++ Scenes/Bullet.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var speed = 10
+var movement = Vector2()
+onready var mouse_pos = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mouse_pos = get_local_mouse_position()
+
+func _physics_process(delta):
+	movement = movement.move_toward(mouse_pos, delta)
+	movement = movement.normalized() * speed
+	position = position + movement
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 const SPEED = 200
 
 var velocity = Vector2.ZERO
+onready var BULLET_SCENE = preload("res://Scenes/Bullet.tscn")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -16,3 +17,13 @@ func _physics_process(delta):
 		velocity = Vector2.ZERO
 
 	move_and_slide(velocity) # Moves body along input vector. Slides against other objects instead of stopping dead. Accounts for delta automatically.
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_mouse_left"):
+		fire()
+
+func fire():
+	var bullet = BULLET_SCENE.instance()
+	bullet.position = get_global_position()
+	get_parent().add_child(bullet)
+	
