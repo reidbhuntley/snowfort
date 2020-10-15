5284c31d3cc6fe596e3311b88478ef5d23a085ce
SometimesJo <jonahcrooks2001@gmail.com>
2020-10-15 19:45:14 -0400

    Added Enemy Spawner
    Added an enemy spawner that spawns mobs in a random cell of the last column. Just a basic prototype. Also tweaked enemy.

--- Scenes/Bullet.gd
+++ Scenes/Bullet.gd
@@ -8,14 +8,19 @@ onready var mouse_pos = null
 func _ready():
 	mouse_pos = get_local_mouse_position()
 
+
 func _physics_process(delta):
 	movement = movement.move_toward(mouse_pos, delta)
 	movement = movement.normalized() * speed
 	position = position + movement
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_Bullet_body_entered(body):
+	if body.is_in_group("mobs"):
+		body.queue_free()
+		queue_free()
---
+++ Scenes/EnemySpawner.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+onready var grid = get_parent().get_node("Grid")
+onready var LASTCOL = grid.grid_bounds.y
+
+onready var MOB_SCENE = preload("res://Scenes/Mobs/RunningEnemy.tscn")
+
+func spawn_mob():
+	var mob = MOB_SCENE.instance()
+	var randomRow = rand_range(1, grid.grid_bounds.x + 1) as int
+	# print("Spawn position is grid(" + LASTCOL as String + ", " + randomRow as String + ")")
+	mob.position = grid.grid_to_world(Vector2(LASTCOL, randomRow))
+	grid.add_child(mob)
