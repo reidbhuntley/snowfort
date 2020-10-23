cad27e84ceb918ec9bc9a87993a46e8dbfb7c633
Brandon <codingbrandon3@gmail.com>
2020-10-22 20:22:56 -0400

    Removed mob background
    made the mob look sleeker

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -43,7 +43,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_mouse_left"):
 		if snowballs > 0:
 			fire()
-			snowballs = snowballs - 1
+			#snowballs = snowballs - 1
 
 	if Input.is_action_pressed("build_wall"):
 		build_wall_progress += delta
